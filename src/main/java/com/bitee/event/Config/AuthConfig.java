8214471baae1bd5e4c58ccd65d2fdbc54c982df7
Bitee-cd <Calebduniya45@gmail.com>
2024-11-22 16:08:43 +0100

    white  listed frontend url

--- src/main/java/com/bitee/event/Config/AuthConfig.java
+++ src/main/java/com/bitee/event/Config/AuthConfig.java
@@ -77,7 +77,7 @@ public class AuthConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:4200")
+                        .allowedOrigins( "http://localhost:4200","https://event-wave-five.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
